6cf3b8c4de095c4c9d79b3d538558e1069931683
festimii <festimi-rrezoni@hotmail.com>
2025-07-01 12:03:40 +0200

    good

--- components/main/features.tsx
+++ components/main/features.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { slideInFromTop } from "@/lib/motion";
+import { motion } from "framer-motion";
+import Image from "next/image";
 import { FEATURES } from "@/constants";
 
 export const Features = () => {
   return (
     <section
       id="features"
-      className="flex flex-col items-center justify-center py-20"
+      className="bg-[#030014]  flex flex-col items-center justify-center py-20"
     >
       <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-10">
         Features
@@ -13,12 +18,12 @@ export const Features = () => {
         {FEATURES.map((feature) => (
           <div
             key={feature.title}
-            className="p-6 border border-[#7042f88b] rounded-lg bg-white bg-opacity-50 backdrop-blur"
+            className="p-6 border border-[#7042f88b] rounded-lg bg-[#030014] "
           >
-            <h2 className="text-xl font-semibold mb-2 text-black">
+            <h2 className="text-xl font-semibold mb-2 text-white">
               {feature.title}
             </h2>
-            <p className="text-gray-800">{feature.description}</p>
+            <p className="text-white">{feature.description}</p>
           </div>
         ))}
       </div>
