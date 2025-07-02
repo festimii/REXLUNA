c7e7531f958a04f0e9f4e0948d9076c5a59fd35b
festimii <festimi-rrezoni@hotmail.com>
2025-07-02 11:43:44 +0200

    test

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -6,12 +6,17 @@ import { StarsCanvas } from "@/components/main/star-background";
 
 export default function ContactPage() {
   const [name, setName] = useState("John Doe");
+
   return (
-    <main className="relative flex min-h-screen items-center justify-center">
+    <main className="relative w-full min-h-screen overflow-hidden">
       <StarsCanvas />
-      <div className="relative z-10 flex flex-col gap-10 md:flex-row md:items-center">
-        <ContactAnimation text={name} />
-        <ContactForm onNameChange={setName} />
+      <div className="relative z-10 flex flex-col h-screen max-w-4xl mx-auto px-4">
+        <div className="flex flex-grow items-center justify-center min-h-[50vh]">
+          <ContactAnimation text={name} />
+        </div>
+        <div className="flex flex-grow items-center justify-center min-h-[50vh]">
+          <ContactForm onNameChange={setName} />
+        </div>
       </div>
     </main>
   );
