0f2093a4c01854ea4547ad33f62d5adb35ea6693
Festim Beqiri <festimi-rrezoni@hotmail.com>
2025-07-06 00:13:49 +0200

    Improve mobile experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
   themeColor: "#030014",
 };
 
