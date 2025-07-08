dfeec965dffa40c24ce2391d7380aeb3452df569
festimii <festimi-rrezoni@hotmail.com>
2025-07-08 15:18:17 +0200

    error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compress: false, // 🔴 disable Next.js built-in compression (Nginx will handle it)
+
   images: {
     domains: ["ui-avatars.com"],
   },
 };
+
 module.exports = nextConfig;
